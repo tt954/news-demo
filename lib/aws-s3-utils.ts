9e202020d99416c7b628c6e9cd8e8011281997bd
tt <ttl.t954@gmail.com>
2025-05-04 22:02:17 -0400

    feat: add s3client and uploadImageToS3 util

---
+++ lib/aws-s3-utils.ts
@@ -0,0 +1,30 @@
+import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
+
+export const s3Client = new S3Client({
+  region: process.env.AWS_REGION,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  },
+});
+
+export async function uploadImageToS3(
+  imageBuffer: Buffer,
+  imagePath: string,
+  contentType: string
+): Promise<string> {
+  try {
+    const command = new PutObjectCommand({
+      Bucket: process.env.AWS_S3_BUCKET_NAME,
+      Key: imagePath,
+      Body: imageBuffer,
+      ContentType: contentType,
+      ACL: "public-read",
+    });
+    await s3Client.send(command);
+    return `https://${process.env.AWS_S3_URL}/${imagePath}`;
+  } catch (error) {
+    console.error("Error uploading image to S3:", error);
+    throw new Error("Failed to upload image to S3");
+  }
+}
