bb5d3a9f4fa41265fe09f31bfafb96224ec2b350
tt <ttl.t954@gmail.com>
2025-05-04 22:19:03 -0400

    refactor: createPost action

--- lib/actions.ts
+++ lib/actions.ts
@@ -1,19 +1,27 @@
 "use server";
 
 import prisma from "@/lib/prisma";
-import { Article } from "@/types/types";
 import { revalidatePath } from "next/cache";
 import slugify from "slugify";
+import { uploadImageToS3 } from "./aws-s3-utils";
 
-export async function createArticle(prevState: any, formData: FormData) {
+interface ActionResponse {
+  success: boolean;
+  message: string | string[];
+}
+
+export async function createPost(
+  prevState: unknown,
+  formData: FormData
+): Promise<ActionResponse> {
   try {
     const title = formData.get("title") as string;
     const image = formData.get("image") as File; // usually a binary blob, next.js automatically parses to File
     const content = formData.get("content") as string;
     const slug = slugify(title, { lower: true });
 
+    /* START validate form data */
     const errors: string[] = [];
-
     if (!title || title.trim().length === 0)
       errors.push("Title cannot be empty");
     if (title.length > 100) errors.push("Title cannot exceed 100 characters");
@@ -22,26 +30,35 @@ export async function createArticle(prevState: any, formData: FormData) {
       errors.push("Content cannot be empty");
     if (content.length > 5000)
       errors.push("Content cannot exceed 5000 characters");
+    if (errors.length > 0) {
+      return { success: false, message: errors };
+    }
+    /* validate form data END */
 
-    if (errors.length > 0) return { message: errors };
+    /* START process image */
+    const imageBuffer = Buffer.from(await image.arrayBuffer()); // process image to buffer
+    const fileExtension = image.name.split(".").pop() || "jpg";
+    const fileName = `${slug}.${fileExtension}`; // LATER: maybe use nanoid, etc. to make file name unique
+    const imageUrl = await uploadImageToS3(imageBuffer, fileName, image.type);
+    /* process image END */
 
-    const buffer = Buffer.from(await image.arrayBuffer());
+    await prisma.article.create({
+      data: {
+        title,
+        text: content,
+        slug,
+        date: new Date(),
+        imageUrl,
+      },
+    });
+    revalidatePath("/news");
 
-    const rawArticleData = { title, slug, image: "", text: content };
-
-    return { message: rawArticleData };
-    // await prisma.article.create({
-    //   data: {
-    //     title,
-    //     text: content,
-    //     slug,
-    //     date: new Date(),
-    //     image: "",
-    //   },
-    // });
-    // revalidatePath("/news");
+    return {
+      success: true,
+      message: "Post created",
+    };
   } catch (error) {
     console.error("Error creating article :>> ", error);
-    return { message: "Error creating article" };
+    return { success: false, message: "Failed to create post" };
   }
 }
--- lib/aws-s3-utils.ts
+++ lib/aws-s3-utils.ts
@@ -10,19 +10,19 @@ export const s3Client = new S3Client({
 
 export async function uploadImageToS3(
   imageBuffer: Buffer,
-  imagePath: string,
+  imageName: string,
   contentType: string
 ): Promise<string> {
   try {
     const command = new PutObjectCommand({
       Bucket: process.env.AWS_S3_BUCKET_NAME,
-      Key: imagePath,
+      Key: imageName,
       Body: imageBuffer,
       ContentType: contentType,
       ACL: "public-read",
     });
     await s3Client.send(command);
-    return `https://${process.env.AWS_S3_URL}/${imagePath}`;
+    return `https://${process.env.AWS_S3_URL}/${imageName}`;
   } catch (error) {
     console.error("Error uploading image to S3:", error);
     throw new Error("Failed to upload image to S3");
